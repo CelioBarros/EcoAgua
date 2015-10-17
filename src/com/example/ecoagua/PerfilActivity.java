371a03ec1a0f38cba1bff2987d2437e499383b70
Tatiana Saturno <tatianassaturno@gmail.com>
2015-10-17 00:26:20 -0300

    abrindo tela de cadastro quando clica em cadastrar morador

--- src/com/example/ecoagua/PerfilActivity.java
+++ src/com/example/ecoagua/PerfilActivity.java
@@ -5,14 +5,36 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
 public class PerfilActivity extends Activity {
+	private Button btnCadastrarMoradorPerfil;
+	private Button btnSair;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_perfil);
+		
+		btnCadastrarMoradorPerfil = (Button) findViewById(R.id.btn_cadastrar_morador_perfil);
+		btnSair = (Button) findViewById(R.id.btn_sair);
+		
+		cadastrarMorador();
+	}
+
+	private void cadastrarMorador() {
+		btnCadastrarMoradorPerfil.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(PerfilActivity.this, CadastroMoradorActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
